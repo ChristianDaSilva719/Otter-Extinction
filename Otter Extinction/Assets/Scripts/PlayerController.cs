59778d1cc7425f5355e73a7953bd89691d3b7e23
vHungryPanda <victorwong346@gmail.com>
2026-06-03 01:18:03 +1200

    Added Player Controller Scipt

---
+++ Otter Extinction/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get player inputs
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        // Normalize movement
+        moveInput.Normalize();
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(moveInput * moveSpeed);
+    }
+}
