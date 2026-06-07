9b108682e678b87e1a03742ef463f59b7b4bfd42
vHungryPanda <victorwong346@gmail.com>
2026-06-07 21:09:59 +1200

    Added Dashing
    Spacebar to Dash

--- Otter Extinction/Assets/Scripts/PlayerController.cs
+++ Otter Extinction/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,29 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private TrailRenderer tr;
+
     [Header("Movement")]
-    [SerializeField] private float moveSpeed = 4.0f;
+    private float moveSpeed = 5.0f;
+
     private Vector2 moveInput;
+    // Dashing
+    [Header("Dashing")]
+    [SerializeField] private float dashCooldown = 0.3f;
+    [SerializeField] private float dashDuration = 0.5f;
+    [SerializeField] private float dashForce = 10.0f;
+
+    private bool canDash = true;
+    private bool isDashing;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
@@ -21,10 +35,36 @@ public class PlayerController : MonoBehaviour
         // Normalize movement
         moveInput.Normalize();
 
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
         rb.AddForce(moveInput * moveSpeed);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        
+        // add velocity direction pressed+ dashforce
+        rb.linearVelocity = moveInput * dashForce;
+        tr.emitting = true;
+
+        yield return new WaitForSeconds(dashDuration);
+        tr.emitting = false;
+        isDashing = false;
+        
+        // Cooldown before dashing again
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
